abf1dc0cce2e6b27b32bfacb87a6039de9c58365
Mike Fogel <mike@fogel.ca>
2008-09-25 23:15:01 +0000

    - upgraded to trac 0.11.1  - only add the packages/bin to the path if it's not already there.  - tested some on cs106l, more needed.

--- common.sh
+++ common.sh
@@ -13,7 +13,7 @@ INSTALL=${TRAC_ROOT}/install
 SITES=${TRAC_ROOT}/sites
 SVN=${HOME}/svn
 TRAC_SITE_PACKAGES=${PKG}/lib/python2.3/site-packages
-TRAC_HTDOCS=${TRAC_SITE_PACKAGES}/Trac-0.11-py2.3.egg/trac/htdocs
+TRAC_HTDOCS=${TRAC_SITE_PACKAGES}/Trac-0.11.1-py2.3.egg/trac/htdocs
 
 EXP_CMD1="export PYTHONPATH=$TRAC_SITE_PACKAGES"
 EXP_CMD2="export PYTHON_EGG_CACHE=$EGG"
@@ -36,18 +36,17 @@ OutputDreamTracInstallReminder
 $EXP_CMD1
 $EXP_CMD2
 $EXP_CMD3
-# FIX THSSSSSSSSSSSSSSSS
-export PATH=$PKG/bin:$PATH
+if [[ "$PATH" != *"$PKG/bin"* ]]; then export PATH=$PKG/bin:$PATH; fi
 
 # Update version information here and DIRs below.
 
-TRAC=http://ftp.edgewall.com/pub/trac/Trac-0.11.tar.gz
+TRAC=http://ftp.edgewall.com/pub/trac/Trac-0.11.1.tar.gz
 GENSHI=http://ftp.edgewall.com/pub/genshi/Genshi-0.5.tar.gz
 SETUPTOOLS=http://peak.telecommunity.com/dist/ez_setup.py
 MYSQLDB=http://switch.dl.sourceforge.net/sourceforge/mysql-python/MySQL-python-1.2.2.tar.gz
 PYGMENTS=http://pypi.python.org/packages/source/P/Pygments/Pygments-0.9.tar.gz
 
-TRACDIR=Trac-0.11
+TRACDIR=Trac-0.11.1
 GENSHIDIR=Genshi-0.5
 MYSQLDBDIR=MySQL-python-1.2.2
 PYGMENTSDIR=Pygments-0.9
